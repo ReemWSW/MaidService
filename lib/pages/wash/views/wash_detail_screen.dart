821c2bc99b2ae02c120b90f5170be5460593bbfc
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-03-04 16:49:22 +0700

    create card detail

--- lib/pages/wash/views/wash_detail_screen.dart
+++ lib/pages/wash/views/wash_detail_screen.dart
@@ -5,6 +5,46 @@ class WashDeatailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('บริการที่ครอบคลุม')));
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('บริการที่ครอบคลุม'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                elevation: 8,
+                child: Container(
+                  padding: const EdgeInsets.all(30),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      textDetail('บริการคลอบครุมบริการซักเสื้อผ้า'),
+                      const Text(
+                        'sssssssssss',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+
+  Text textDetail(String text) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 18),
+    );
   }
 }
